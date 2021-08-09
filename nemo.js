663a5af2f1dfe667d9310eb0464c6778f1aed08e
farhanpro <shaihfarhan549@gmai.com>
2021-08-09 11:45:08 +0530

    Day 1

---
+++ nemo.js
@@ -0,0 +1,9 @@
+const All =["Farhan","harshal","Diraj","Uzaif","Tausif","Aherar","Tausif bhai","lucy"];
+for(let i =0 ; i < All.length;i++){
+    if(All[i] === "lucy"){
+        console.log("Found lucy");
+    }
+    else{
+        console.log("Not Found");
+    }
+}
---
+++ porb2.js
@@ -0,0 +1,9 @@
+const lund = ['farhan','harshal','diraj','huzzaif','tauseef','aherar'];
+for(let i=0;i<=lund.length;i++){
+    if(lund[i] === 'tauseef'){
+        console.log(lund[i] +" milgya");
+        break;
+    }
+    else{
+    console.log(' nahi mila');
+}}
